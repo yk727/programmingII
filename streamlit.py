38a3f6948b18de4d68e646902cd7924a826807de
yk727 <120370373+yk727@users.noreply.github.com>
2022-12-12 19:11:31 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,8 +5,8 @@ import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
-
-os.chdir(r'C:\Users\Kim\Desktop\Georgetown\Programming II\Final Project')
+os.path.relpath(r'C:\Users\Kim\Desktop\Georgetown\Programming II\Final Project')
+# os.chdir(r'C:\Users\Kim\Desktop\Georgetown\Programming II\Final Project')
 
 s = pd.read_csv('social_media_usage.csv')
 print(s)
